d0e6f7102c591c3c4e4cc069e397195db9e78c26
Carlos Eduardo <cadu4duarte@gmail.com>
2022-12-22 16:02:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useFetch } from "./hooks/useFetch";
+
+type Repository = {
+  full_name: string;
+  description: string;
+}
+
+function App() {
+  const { data: repositories } = useFetch<Repository[]>("https://api.github.com/users/cadu4duarte/repos");
+
+  return (
+    <ul>
+      {repositories?.map(repo => {
+        return(
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default App
