c85b1085b3c0a693c5ba0f6378afb680ca261486
Carlos Eduardo <cadu4duarte@gmail.com>
2022-12-23 12:56:03 -0300

    feat: api consume with axios

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,15 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const api = axios.create({
+    baseURL: "https://api.github.com"
+})
+
 export function useFetch<T = unknown>(url: string) {
     const [data, setData] = useState<T | null>(null);
+    const [isFetching, setIsFetching] = useState(true);
 
     useEffect(() => {
-        axios.get(url)
+        api.get(url)
             .then(response => {
                 setData(response.data)
             })
-    }, [])
+            .finally(() => {
+                setIsFetching(false);
+            })
+    }, []);
 
-    return { data }
+    return { data, isFetching }
 }
